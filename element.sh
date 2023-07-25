191f893d7f7b2bf566a427064f28dba0d26ea180
Maria Lobillo-Santos <maria.lobillo.santos@gmail.com>
2023-07-25 11:03:34 +0200

    feat: check if argument is a number, getting info from element

--- element.sh
+++ element.sh
@@ -9,3 +9,26 @@ then
   exit
 fi
 
+if [[ $1 =~ ^[1-9]=$ ]]
+then
+  info=$($PSQL "select 
+  e.atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius 
+  from elements e 
+  join properties p on e.atomic_number=p.atomic_number 
+  join types t on t.type_id=p.type_id 
+  where atomic_number = '$1'")
+else
+  info=$($PSQL "select 
+  e.atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius 
+  from elements e 
+  join properties p on e.atomic_number=p.atomic_number 
+  join types t on t.type_id=p.type_id 
+  where name = '$1' or symbol = '$1'")
+fi
+
+if [[ -z $info ]]
+then
+  echo -e "I could not find that element in the database."
+  exit
+fi
+
